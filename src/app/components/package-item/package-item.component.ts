b19ced62a981e92a35c62fd735765f03428fe321
Yassine Klilich <klilichyassine.contact@gmail.com>
2023-07-01 20:04:47 +0100

    feat: show GitHub issues and pulls refact: use ElementRef to get the canvas element instead of using the id

--- src/app/components/package-item/package-item.component.ts
+++ src/app/components/package-item/package-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, ViewChild, ElementRef } from '@angular/core'
 import { IPackage } from 'src/app/model/IPackage'
 import { Chart } from 'chart.js'
 
@@ -9,7 +9,26 @@ import { Chart } from 'chart.js'
 })
 export class PackageItemComponent {
   @Input() package!: IPackage
+  @ViewChild("chartCanvas") chartCanvas!: ElementRef<HTMLCanvasElement>
   chart!: Chart
+  issues: number = 0
+  pulls: number = 0
+  hideGithubData: boolean = true
+
+  async ngOnInit() {
+    const [pulls, issues] = await Promise.all([
+      this.fetchApi(this.package.name, "pulls"),
+      this.fetchApi(this.package.name, "issues"),
+    ])
+    if (pulls !== null && issues !== null) {
+      this.pulls = pulls
+      this.issues = issues - pulls
+      this.hideGithubData = false
+    }
+    else {
+      this.hideGithubData = true
+    }
+  }
 
   ngAfterViewInit() {
     const days: Array<string> = this.package.downloads.map((item) => item.day)
@@ -17,7 +36,7 @@ export class PackageItemComponent {
       (item) => item.downloads
     )
 
-    this.chart = new Chart(`package-${this.package.name}`, {
+    this.chart = new Chart(this.chartCanvas.nativeElement, {
       type: 'line',
       data: {
         labels: days,
@@ -78,5 +97,28 @@ export class PackageItemComponent {
 
   onClickResetZoom() {
     this.chart.resetZoom()
+  }
+
+  private async fetchApi(repo: string, resource: string) {
+    try {
+      const res = await window.fetch(`https://api.github.com/repos/yassine-klilich/${repo}/${resource}?per_page=1`)
+      const link = res.headers.get('Link')
+      const count = this.parseLastPage(link)
+      if (link != null) return count
+      const body = await res.json()
+      return body.length
+    } catch (e) {
+      return null
+    }
+  }
+
+  private parseLastPage(linkHeader: string | null) {
+    if (!linkHeader) return null
+    const links = linkHeader.split(',')
+    const lastLink = links.find(link => link.match(/rel="last"/))
+    if (lastLink == null) return 0
+    const d = lastLink.match(/[^_]page=(\d+)/)
+    if (d == null) return 0
+    return Number(d[1])
   }
 }
